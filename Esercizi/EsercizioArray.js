d9c3ca7c0ffdb135f8eec551356c94cf53963fba
saverio <saveriodinapoli99@gmail.com>
2024-05-03 11:39:10 +0200

    esercizi array in JavaScript

---
+++ Esercizi/EsercizioArray.js
@@ -0,0 +1,85 @@
+let libri = [{
+    titolo: "Il Signore degli Anelli",
+    autore: "J.R.R. Tolkien",
+    annoDiPubblicazione: 1954,
+    letto: true
+},
+{
+    titolo: "Harry Potter e la Pietra Filosofale",
+    autore: "J.K. Rowling",
+    annoDiPubblicazione: 1997,
+    letto: false
+},
+{
+    titolo: "1984",
+    autore: "George Orwell",
+    annoDiPubblicazione: 1949,
+    letto: true
+}];
+function addBook(libro){
+    libri.push(libro);
+}
+function visualizzaLibri() {
+    console.log("Lista dei libri:");
+    libri.forEach(function(libro) {
+        console.log("Titolo: " + libro.titolo + ", Autore: " + libro.autore + ", Letto: " + (libro.letto ? 'Sì' : 'No'));
+    });
+}
+ visualizzaLibri();
+
+
+ function cercaLibro(titolo) {
+    const libroTrovato = libri.find(function(libro) {
+        return libro.titolo.toLocaleLowerCase === titolo.toLocaleLowerCase;
+    });
+    if (libroTrovato) {
+        console.log("Il libro " + titolo + " è stato trovato");
+    } else {
+        console.log("Il libro " + titolo + " non è stato trovato");
+    }
+}
+cercaLibro("1984");
+
+function rimuoviLibro(titolo) {
+    const libroDaRimuovere = libri.findIndex(function(libro) {
+        return libro.titolo === titolo;
+    });
+    if (libroDaRimuovere !== -1) {
+        libri.splice(libroDaRimuovere, 1);
+        console.log("Il libro " + titolo + " è stato rimosso");
+    } else {
+        console.log("Il libro " + titolo + " non è stato rimosso");
+    }
+}
+    rimuoviLibro("Il Signore degli Anelli")
+   
+    function visualizzaLetti() {
+        const libroLetto = libri.filter(function(libro) {
+            return libro.letto;
+        });
+        console.log("Libri letti : ");
+        libroLetto.forEach(function(libro) {
+            console.log(libro.titolo);
+        });
+    }
+    visualizzaLetti();
+   
+    function visualizzaNonLetti() {
+        const libroNonLetto = libri.filter(function(libro) {
+            return !libro.letto;
+        });
+        console.log("Libri non letti : ");
+        libroNonLetto.forEach(function(libro) {
+            console.log(libro.titolo);
+        });
+    }
+    visualizzaNonLetti();
+
+    const libro1 = {
+        titolo: "A Game of Thrones",
+        autore: "George R.R. Martin",
+        annoDiPubblicazione: 1996,
+        letto: true
+    };
+    addBook(libro1);
+    visualizzaLibri();
---
+++ Esercizi/prova.js
@@ -0,0 +1,38 @@
+/*var x = 10.0;
+var y = 10.0;
+var result = x==y
+if(x==y){
+    console.log("sono uguali")
+}else{
+    console.log("Non sono uguali")
+}
+console.log("x : " + x);
+console.log("y : " + y);
+function somma(a,b){
+    console.log(`somma: ${a} + ${b} = ${a+b}`);
+    return a+b;
+}
+ var c = somma(2,3);
+ console.log(c)
+  let players = [0,1,2,3,4];
+  console.log(players[6]);
+  let x =[[1,2,3],[4,5,6]];
+  console.log(x[0][1]); 
+  const array1 = [1,2,3];
+  const array2 = [4,5,6];
+  // ... i tre puntini sospensivi sono l'operatore spread 
+  const arrays =[...array1, ...array2];
+  console.log(array1);
+  console.log(array2);
+  console.log(arrays);
+let persona = {
+    nome : 'mario',
+    eta : 30,
+    saluta : 
+    function(){
+       console.log('ciao mi chiamo ' + this.nome+ ' ' + 'ho'+ ' ' + this.eta + ' ' + 'anni')
+    }
+    }
+ persona.saluta();
+ console.log(persona.eta);
+ console.log(persona["nome"])*/ 
